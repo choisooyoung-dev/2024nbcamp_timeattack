9d895b40ed8a5f9be560f18d9e68beafa1e63cf5
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-06 12:11:04 +0900

    🎉 Init: TypeORM 추가, 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configModuleValidationSchema } from './configs/env-validation.config';
+import { typeOrmModuleOptions } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configModuleValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
